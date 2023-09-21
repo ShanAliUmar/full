8e371d35ca687d34df0615087fc750ebf1a8f3f7
ShanAliUmar <shanumar.dev@gmail.com>
2023-09-21 09:01:57 +0500

    fsdhuh

--- index.js
+++ index.js
@@ -825,12 +825,23 @@
 
 
 
+const shagggggi ={
+    name:"shan umar",
+    className:"10th",
+}
+
+function info(ratting, quality) {
+    console.log(`My  name is ${this.name} . ratting = ${ratting} and quality = ${quality}`);
+}
 
 
+//  const fun = info.bind(shagggggi)
+// fun()
 
-
-
-
+info.call(shagggggi, 5, "best")
+info.apply(shagggggi,[ 5, "best"])
+let arr = [ 5, "best"]
+info.apply(shagggggi,arr)
 
 
 
